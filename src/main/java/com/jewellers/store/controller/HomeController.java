be712f410d1e466b491aa289d772ce2894db2673
Sadiyatazeen <sadiyatazeen14@gmail.com>
2024-06-24 15:02:30 +0530

    Code changed in homecontroller (Sadiya)

--- src/main/java/com/jewellers/store/controller/HomeController.java
+++ src/main/java/com/jewellers/store/controller/HomeController.java
@@ -1,16 +1,38 @@
 package com.jewellers.store.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class HomeController {
 
-	@GetMapping(value="/")
+	
+	@Value("${store.name}")
+	private String storeName;
+	
+	@GetMapping(value="/login")
 	public String login() {
 		//	System.out.println("Called");
 	    return  "login";
 	}
+	
+	@GetMapping(value="/")
+	public String home() {
+			System.out.println("Called Home");
+	    return  "home";
+	}
+	
+	@GetMapping(value="/test")
+	public String test(Model model ) {
+			System.out.println("Called test");
+			System.out.println(storeName);
+			model.addAttribute("storeName",storeName);
+	    return  "test";
+	}
+	
+	
 }
