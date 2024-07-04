22fe339b9f7175468b9ae7248584ee802d9c704b
rishabhbansall <142716534+rishabhbansall@users.noreply.github.com>
2024-07-04 17:09:35 +0530

    constants defined for database connection

---
+++ src/main/java/com/jewellers/store/util/Constant.java
@@ -0,0 +1,10 @@
+package com.jewellers.store.util;
+
+public class Constant {
+
+	public static final String DB_URL="jdbc:mysql://localhost:3306/jewellery" ;
+	public static final String DB_DRIVER_NAME="com.mysql.cj.jdbc.Driver" ;
+	public static final  String DB_USER_NAME ="root";
+	public static final String DB_PASSWORD="rishabh@1234";
+	
+}
